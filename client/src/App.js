32635b876a3d491d26e210af821506308f78646b
JuanPablo-Llorente <juanpablollorentej@gmail.com>
2022-07-12 13:43:33 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+// Dependenecies
+import React from "react";
+// Files
+import "./App.css";
+
+
+function App()
+{
+    return(
+        <React.Fragment>
+            
+        </React.Fragment>
+    );
+};
+
+
+export default App;
